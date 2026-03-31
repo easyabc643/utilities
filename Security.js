b07a4d0894639d7526fa088627011f83b2c62d28
easyabc643 <2583522566@student.cms.k12.nc.us>
2026-03-31 14:38:18 -0400

    Implement security setup for secure elements

---
+++ Security.js
@@ -0,0 +1,20 @@
+let secureElements;
+let secureContent;
+let unlocked = false;
+
+function setup(){
+  const start = new Date();
+  secureElements = document.getElementByClass("secure");
+  secureContent = document.getElementByClass("secure-content");
+  secureContent.forEach(s => {s.disabled = true;})
+  const button = document.createElement("button");
+  button.textContent = "Prove I'm not a robot";
+  function handleClick(){
+    const end = new Date();
+    if(end - start > 500){
+      unlocked = true;
+      secureContent.forEach(s => {s.disabled = false;});
+    }
+  }
+}
+window.onload = setup();
